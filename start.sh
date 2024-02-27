1e1c1b0bf1a8bd122f107e612b0e3698bd446fd3
shimguh <shimguh@naver.com>
2024-02-27 16:34:11 +0900

    nohup

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nohup java -jar ~/app/rest-sb/rest-service-0.2.0-SNAPSHOT.jar >> ~/app/rest-sb/log.log &
+nohup java -jar ~/app/rest-sb/rest-service-0.0.1-SNAPSHOT.jar >> ~/app/rest-sb/log.log &
 echo $! > ~/app/rest-sb/pid.file
